1e1ea1cff0b7c5eb546f47361c4f88c24f16e53e
xi4f3i <xi4f3i@gmail.com>
2026-02-04 14:39:26 +0800

    Add parsing result structs

--- src/parser/css/parser.rs
+++ src/parser/css/parser.rs
@@ -1,2 +1,14 @@
+use crate::parser::css::tokenizer::Tokenizer;
+
 /// https://drafts.csswg.org/css-syntax/#parsing
-pub(crate) struct CSSParser {}
+pub(crate) struct CSSParser<'a> {
+    tokenizer: Tokenizer<'a>,
+}
+
+impl<'a> CSSParser<'a> {
+    pub(crate) fn new(input: &'a str) -> CSSParser<'a> {
+        CSSParser {
+            tokenizer: Tokenizer::new(input),
+        }
+    }
+}
--- src/parser/css/result.rs
+++ src/parser/css/result.rs
@@ -1,16 +1,51 @@
+use crate::parser::css::tokenizer::Token;
+
 /// https://drafts.csswg.org/css-syntax/#css-tree
-struct Stylesheet {
-    rules: Vec<Rule>,
+struct Stylesheet<'a> {
+    rules: Vec<Rule<'a>>,
 }
 
 /// https://drafts.csswg.org/css-syntax/#css-rule
-enum Rule {
-    At(AtRule),
-    Qualified(QualifiedRule),
+enum Rule<'a> {
+    At(AtRule<'a>),
+    Qualified(QualifiedRule<'a>),
 }
 
 /// https://drafts.csswg.org/css-syntax/#at-rule
-struct AtRule {}
+struct AtRule<'a> {
+    name: String,
+    prelude: Vec<ComponentValue<'a>>,
+    declarations: Vec<Declaration<'a>>,
+    children: Vec<Rule<'a>>,
+}
 
 /// https://drafts.csswg.org/css-syntax/#qualified-rule
-struct QualifiedRule {}
+struct QualifiedRule<'a> {
+    prelude: Vec<ComponentValue<'a>>,
+    declarations: Vec<Declaration<'a>>,
+    children: Vec<Rule<'a>>,
+}
+
+/// https://drafts.csswg.org/css-syntax/#declaration
+struct Declaration<'a> {
+    name: String,
+    values: Vec<ComponentValue<'a>>,
+    unset: bool,
+}
+
+/// https://drafts.csswg.org/css-syntax/#component-value
+enum ComponentValue<'a> {
+    PreservedToken(Token<'a>),
+    Function(Function<'a>),
+    SimpleBlock(SimpleBlock<'a>),
+}
+
+struct Function<'a> {
+    name: String,
+    values: Vec<ComponentValue<'a>>,
+}
+
+struct SimpleBlock<'a> {
+    token: Token<'a>,
+    values: Vec<ComponentValue<'a>>,
+}
